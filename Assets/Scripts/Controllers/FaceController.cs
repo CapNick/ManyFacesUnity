39b228558f7dca5b8d5e801add944d4b42774bcf
Nick Hird <nickdh94@gmail.com>
2017-11-03 02:41:58 +0000

    Added project to github from the original, import was not feasable. DLL's missing will need use fo git submoudle from uni repository

---
+++ Assets/Scripts/Controllers/FaceController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceController : MonoBehaviour {
+	
+	private List<Emotion> _animations = new List<Emotion>();
+
+	//looking variables
+	[SerializeField]
+	private Transform _lookingTarget;
+
+	[SerializeField] private Transform _leftEye;
+	[SerializeField] private Transform _rightEye;
+
+	void Update()  {
+		_leftEye.LookAt(_lookingTarget.position);
+		_rightEye.LookAt(_lookingTarget.position);
+	}
+
+	private void OnDrawGizmosSelected() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(_leftEye.position, _lookingTarget.position);
+		Gizmos.DrawLine(_rightEye.position, _lookingTarget.position);
+		Gizmos.DrawWireSphere(_lookingTarget.position, 0.5f);
+	}
+
+
+	public void Happy() {
+		
+	}
+
+	public void Sad() {
+		
+	}
+
+	public void Confused() {
+		
+	}
+
+}
---
+++ Assets/Scripts/Utils/CameraFeed.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using UnityEngine;
+
+//emgu.cv imports
+using Emgu.CV;
+using Emgu.CV.Cvb;
+using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.VideoSurveillance;
+using UnityEditor;
+
+public class CameraFeed : MonoBehaviour {
+
+	private Capture cap;
+	private CascadeClassifier _cascadeClassifier;
+
+	
+	void Start() 
+	{
+		cap = new Capture();
+				
+		_cascadeClassifier = new CascadeClassifier("Assets/Resources/haarcascade_frontalface_default.xml");									
+
+		
+	}
+
+	public void Update() {
+		GetImage();
+	}
+	
+
+	private void GetImage()
+	{
+		using (Image<Bgr, byte> nextFrame = cap.QueryFrame())
+		{
+			if (nextFrame != null)
+			{
+				// there's only one channel (greyscale), hence the zero index
+				//var faces = nextFrame.DetectHaarCascade(haar)[0];
+				Image<Gray, byte> grayFrame = nextFrame.Convert<Gray, byte>();
+				Size imageSize = new Size();
+				var faces = _cascadeClassifier.DetectMultiScale(grayFrame, 1.1, 4, imageSize, Size.Empty);
+
+				
+				
+				foreach (var face in faces)
+				{
+					nextFrame.Draw(face, new Bgr(0,double.MaxValue,0), 3);
+				}
+				MemoryStream mem = new MemoryStream();
+				nextFrame.Bitmap.Save(mem, nextFrame.Bitmap.RawFormat);
+				
+				Texture2D camera = new Texture2D(1920, 1080);
+				camera.LoadImage(mem.ToArray());
+				
+				GetComponent<Renderer>().material.mainTexture = camera;
+			}
+		}
+	}
+	void OnApplicationQuit() {
+	//releasing the camera after use
+		if (cap != null) {
+			cap.Dispose();
+		}
+	}
+}
