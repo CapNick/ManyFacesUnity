4b1237fb1382dfe49d373749ed46e46df642c7f2
Nick Hird <nickdh94@gmail.com>
2018-02-22 20:51:13 +0000

    Added Face Controller Added face2 class

---
+++ Assets/Scripts/Controllers/Face2.cs
@@ -0,0 +1,23 @@
+﻿using Models;
+using UnityEngine;
+
+namespace Controllers {
+	public class Face2 : MonoBehaviour {
+
+
+		public Staff staff;
+		public Mesh FaceMesh;
+	
+	
+	
+		// Use this for initialization
+		void Start () {
+		
+		}
+	
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
--- Assets/Scripts/Controllers/FaceController.cs
+++ Assets/Scripts/Controllers/FaceController.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Models;
 using UnityEngine;
 
-public class FaceController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+namespace Controllers {
+	public class FaceController : MonoBehaviour {
+		public GameObject FacePrefab;
+		public GameObject[] Faces;
+		public int MembersOfStaff;
 		
-	}
+		private string _uri = "https://dev.capnick.co.uk/faces.json";
+		private string file = "faces.json";
+		private StaffData _staffData;  
+
+		
+		public void Awake() {
+		
+		}
+
+		// Use this for initialization
+		void Start () {
+			_staffData = new StaffData(file);
+			_staffData.LoadAllData();
+			MembersOfStaff = _staffData.Members.Count;
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		public void ReloadFaces() {
+			Debug.Log("Faces Updating...");
+			Debug.Log("Clearing Faces...");
+			foreach (GameObject face in Faces) {
+				Destroy(face);
+			}
+		}
 		
 	}
 }
